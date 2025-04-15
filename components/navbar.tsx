a7bbdf3df6ad8b1dba9b27d4a79db166798b7203
Afolabi Ridwan Damilare <damilare791@gmail.com>
2025-04-15 21:15:26 +0100

    feat: enhance navbar with mobile responsiveness and dynamic navigation items

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,49 +10,54 @@ import {
 } from '@/components/ui/navigation-menu'
 
 import { cn } from '@/lib/utils'
+import { LogOutIcon, Menu } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
+const navItems = [
+  { name: 'Home', link: '/' },
+  { name: 'Blogs', link: '/blogs' },
+  { name: 'Schedules', link: '/schedules' },
+  { name: 'Draft', link: '/draft' },
+]
+
 export default function NavbarComp() {
+  const pathname = usePathname()
+
   const [isOpen, setIsOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
 
   return (
     <div className='bg-transparent fixed top-0 left-0 z-20 h-[5rem] w-full flex flex-row justify-between items-center px-5 py-3 lg:px-16 lg:py-5'>
-      <h1 className='text-3xl font-semibold '>Sparks & stories</h1>
+      <Link href='/'>
+        <h1 className='text-3xl font-semibold capitalize '>Sparks & stories</h1>
+      </Link>
+
       <NavigationMenu className='lg:block hidden'>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href='/' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/blogs' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Blogs
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/schedules' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Schedules
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/draft' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Draft
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+        <NavigationMenuList className='gap-5'>
+          {navItems.map((item) => {
+            const isActive = pathname === item.link
+
+            return (
+              <NavigationMenuItem key={item.name}>
+                <Link href={item.link} legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={cn(
+                      navigationMenuTriggerStyle(),
+                      isActive ? 'bg-gray-600 text-white' : ''
+                    )}
+                  >
+                    {item.name}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )
+          })}
         </NavigationMenuList>
       </NavigationMenu>
-      <div className='relative'>
+      <div className='relative flex flex-row justify-start items-center gap-3'>
         <Avatar
           onMouseDown={() => setIsOpen(!isOpen)}
           className={cn(
@@ -66,6 +71,14 @@ export default function NavbarComp() {
           />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
+
+        <button
+          type='button'
+          className='lg:hidden text-4xl text-gray-500 hover:text-gray-700'
+          onClick={() => setIsMobile(!isMobile)}
+        >
+          <Menu />
+        </button>
 
         {isOpen && (
           <div className='absolute top-16 right-2 w-[250px] rounded-md p-2 bg-white ring-1 ring-gray-400 flex flex-col justify-start items-start'>
@@ -95,6 +108,50 @@ export default function NavbarComp() {
           </div>
         )}
       </div>
+
+      {isMobile && (
+        <div className='fixed top-0 left-0 h-screen w-[80vw] bg-black text-white p-5 z-30'>
+          <div className='flex flex-col justify-between items-start h-[80vh]'>
+            <Link href='/'>
+              <h1 className='text-3xl capitalize font-semibold text-white'>
+                Sparks & stories
+              </h1>
+            </Link>
+
+            <div className='flex flex-col justify-start items-start gap-3 w-full'>
+              {navItems.map((item) => {
+                const isActive = pathname === item.link
+
+                return (
+                  <Link
+                    key={item.name}
+                    href={item.link}
+                    legacyBehavior
+                    passHref
+                  >
+                    <span
+                      className={cn(
+                        `text-white h-12 w-full rounded-md flex justify-start items-center px-2 text-xl`,
+                        isActive ? 'bg-gray-600' : 'hover:bg-gray-400'
+                      )}
+                    >
+                      {item.name}
+                    </span>
+                  </Link>
+                )
+              })}
+            </div>
+
+            <button
+              type='button'
+              className='flex flex-row justify-start items-center gap-3 w-full rounded-md h-12 hover:bg-gray-400'
+            >
+              {' '}
+              <LogOutIcon /> Logout
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
